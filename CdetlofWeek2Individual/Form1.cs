e59d3b2c24250c29be5ec70f6895db962dfe2747
cd4242 <detlof@email.com>
2017-07-20 13:24:34 -0500

    These are the files for my for loop project

---
+++ CdetlofWeek2Individual/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CdetlofWeek2Individual
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            
+          
+
+             
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //creates for loop to start at 10 and increase by 3 until it reaches one hundred once the button is clicked
+            for (int i = 10; i <= 100; i += 3)
+            {
+                //allows textbox to display for loop and puts a space between each number 
+                textBox1.Text += i.ToString() + " ";
+
+            }
+        }
+    }
+}
